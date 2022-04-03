339a730f7d8c21e52ce6964a3f4b71f3e5190ba0
BuildTools <unconfigured@null.spigotmc.org>
2022-04-03 15:20:17 +0100

    Page Format Update

--- Assets/js/nav.js
+++ Assets/js/nav.js
@@ -3,12 +3,15 @@ const navToggle = document.querySelector(".moblie-nav-toggle");
 
 navToggle.addEventListener('click', () => {
     const visibility = navPrimary.getAttribute('data-visible');
+    console.log("Click");
     
     if (visibility === "false") {
         navPrimary.setAttribute('data-visible', true);
         navToggle.setAttribute('aria-expanded', true);
+        console.log("Set True");
     } else if (visibility === "true") {
         navPrimary.setAttribute('data-visible', false);
         navToggle.setAttribute('aria-expanded', false);
+        console.log("Set False");
     }
 })
